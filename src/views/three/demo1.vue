42f38a6b146ea20a2db44b64dc1d05038f5a4ee7
zhangruixue <18729255769@163.com>
2024-03-23 19:23:40 +0800

    :sparkles: three-demo1 重写：初始化流程 && 设置阻尼 && 设置三维坐标系

--- src/views/three/demo1.vue
+++ src/views/three/demo1.vue
@@ -10,39 +10,54 @@
 <script setup>
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 let container;
 
 
 onMounted(() => {
 
-  function animate() {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-  }
-
-
   const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
+  camera.position.z = 10;
+  camera.position.y = 1;
+  camera.position.x = 1;
+  camera.lookAt(0,0,0);
 
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize(window.innerWidth, window.innerHeight);
   container = document.querySelector('#demo1');
   container.appendChild( renderer.domElement );
 
-  const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  const cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const box = new THREE.BoxGeometry(1,1,1);
 
-  camera.position.z = 5;
+  const cube = new THREE.Mesh(box, material);
+  scene.add(cube);
 
+  const axesHelper = new THREE.AxesHelper(3);
+  scene.add(axesHelper)
+
+  const control = new OrbitControls(camera, renderer.domElement);
+  scene.add(control);
+  control.autoRotate = true;
+  control.enableDamping = true;
+  control.update();
+
+  renderer.render(scene, camera)
+
+
+  function animate() {
+    requestAnimationFrame(animate);
+    control.update();
+
+    renderer.render(scene, camera)
+
+  }
 
   animate();
+
 });
 
 </script>
---
+++ src/views/three/demo2.vue
@@ -0,0 +1,57 @@
+<template>
+  <div className="index-vue">
+
+    <div id="demo2" className="three-demo2"></div>
+
+  </div>
+</template>
+
+
+<script setup>
+
+import * as THREE from 'three';
+
+let container;
+
+
+onMounted(() => {
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  container = document.querySelector('#demo2');
+  container.appendChild( renderer.domElement );
+
+
+  const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+  camera.position.set(0, 0, 50);
+  camera.lookAt(0, 0, 0);
+
+
+  const scene = new THREE.Scene();
+  const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+
+  const points = [];
+  points.push( new THREE.Vector3( - 10, 0, 0 ) );
+  points.push( new THREE.Vector3( 0, 10, 0 ) );
+  points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+  const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+  const line = new THREE.Line( geometry, material );
+
+  scene.add( line );
+
+
+  renderer.render( scene, camera );
+
+});
+
+</script>
+
+<style lang="scss" scoped>
+.three-demo2 {
+  width: 100%;
+  height: 100%;
+}
+</style>
