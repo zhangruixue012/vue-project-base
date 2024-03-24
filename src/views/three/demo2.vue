ebb5fd7b9bcfe58d6674e5da238663a362c7b41d
zhangruixue <18729255769@163.com>
2024-03-24 20:31:22 +0800

    :sparkles: 几何体顶点 && 索引 && 面

--- src/views/three/demo2.vue
+++ src/views/three/demo2.vue
@@ -1,7 +1,7 @@
 <template>
-  <div className="index-vue">
+  <div class="index-vue">
 
-    <div id="demo2" className="three-demo2"></div>
+    <div id="demo2" class="three-demo2"></div>
 
   </div>
 </template>
@@ -10,48 +10,71 @@
 <script setup>
 
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {GUI} from 'three/examples/jsm/libs/lil-gui.module.min.js';
 
 let container;
 
-
 onMounted(() => {
-
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  container = document.querySelector('#demo2');
-  container.appendChild( renderer.domElement );
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-
-  const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-  camera.position.set(0, 0, 50);
+  const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 10;
+  camera.position.y = 1;
+  camera.position.x = 1;
   camera.lookAt(0, 0, 0);
 
+  container = document.querySelector('#demo2');
+  container.appendChild(renderer.domElement);
 
   const scene = new THREE.Scene();
-  const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+  // 创建缓冲区几何体
+  const geometry = new THREE.BufferGeometry();
+
+  // 创建顶点数据
+  const vertices = new Float32Array([
+    -1, -1, 0,
+    1, -1, 0,
+    1, 1, 0,
+   -1, 1, 0
+  ]);
+  // 将顶点数据设置到几何体上
+  geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+
+  const indices = new Uint16Array([0,1,2,2,3,0]);
+  geometry.setIndex(new THREE.BufferAttribute(indices, 1));
+
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    // side: THREE.DoubleSide
+    wireframe: true
+  });
+  const plane = new THREE.Mesh(geometry, material);
+  console.log('geometry:', geometry)
+  scene.add(plane);
+
+  const axesHelper = new THREE.AxesHelper(10);
+  scene.add(axesHelper)
 
 
-  const points = [];
-  points.push( new THREE.Vector3( - 10, 0, 0 ) );
-  points.push( new THREE.Vector3( 0, 10, 0 ) );
-  points.push( new THREE.Vector3( 10, 0, 0 ) );
+  const control = new OrbitControls(camera, renderer.domElement);
+  control.update();
+  control.addEventListener('change', function () {
+    renderer.render(scene, camera); //执行渲染操作
+  });
 
-  const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-  const line = new THREE.Line( geometry, material );
-
-  scene.add( line );
-
-
-  renderer.render( scene, camera );
-
-});
+  renderer.render(scene, camera);
+})
 
 </script>
+
 
 <style lang="scss" scoped>
 .three-demo2 {
   width: 100%;
   height: 100%;
+
 }
 </style>
