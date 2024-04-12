fbe374e2dd0a3a228fa3c5595a92b32bf79c55f3
zhangruixue <18729255769@163.com>
2024-04-12 17:47:31 +0800

    :sparkles: 3D贺卡

---
+++ src/views/three/demo12.vue
@@ -0,0 +1,31 @@
+<!--圣诞节3D贺卡-->
+<template>
+  <div class="index-vue">
+    <div id="demo11" class="three-demo11"></div>
+  </div>
+</template>
+
+
+<script setup>
+
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
+import * as dat from "dat.gui";
+
+
+let container;
+
+onMounted(() => {
+
+})
+
+</script>
+
+
+<style lang="scss" scoped>
+.three-demo10 {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/three/demoExample.vue
+++ src/views/three/demoExample.vue
@@ -1,146 +1,393 @@
-<!--聚光灯-->
 <template>
-  <div class="index-vue">
-    <div id="demoExample" class="three-demoExample"></div>
+  <div id="example">
+    <div class="scenes" :style="{transform: `translate3d(0, ${-index * 100}vh, 0)`}">
+      <div v-for="item in scenes" style="width: 100vw; height: 100vh">
+        <h1 style="padding: 100px 50px; font-size: 50px; color: #fff">
+          {{ item.text }}
+        </h1>
+      </div>
+    </div>
   </div>
+
 </template>
 
-
 <script setup>
-
-import * as THREE from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
-import * as TWEEN from 'three/examples/jsm/libs/tween.module.js';
-import * as dat from "dat.gui";
-
+import * as THREE from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {GLTFLoader} from "three/addons/loaders/GLTFLoader";
+import {DRACOLoader} from "three/addons/loaders/DRACOLoader";
+import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader";
+import {Water} from "three/examples/jsm/objects/Water2";
+import gsap from "gsap";
+import {ref} from "vue";
+import useAppStore from '@/store/modules/app'
 
 let container;
-
 onMounted(() => {
-  // 1、创建场景
-  const scene = new THREE.Scene();
+  container = document.querySelector('#example');
+  container.appendChild(renderer.domElement);
 
-// 2、创建相机
+  const store = useAppStore();
+  store.closeSideBar();
+})
+
+
+  // 初始化场景
+  const scene = new THREE.Scene();
+  // 初始化相机
   const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
   );
+  camera.position.set(-3.23, 2.98, 4.06);
+  camera.updateProjectionMatrix();
+  // 初始化渲染器
+  const renderer = new THREE.WebGLRenderer({
+    // 设置抗锯齿
+    antialias: true,
+  });
 
-// 设置相机位置
-  camera.position.set(0, 0, 10);
-  scene.add(camera);
-
-  const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
-  const material = new THREE.MeshStandardMaterial();
-  const sphere = new THREE.Mesh(sphereGeometry, material);
-// 投射阴影
-  sphere.castShadow = true;
-
-  scene.add(sphere);
-
-// // 创建平面
-  const planeGeometry = new THREE.PlaneGeometry(50, 50);
-  const plane = new THREE.Mesh(planeGeometry, material);
-  plane.position.set(0, -1, 0);
-  plane.rotation.x = -Math.PI / 2;
-// 接收阴影
-  plane.receiveShadow = true;
-  scene.add(plane);
-
-// 灯光
-// 环境光
-  const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
-  scene.add(light);
-
-  const smallBall = new THREE.Mesh(
-      new THREE.SphereGeometry(0.1, 20, 20),
-      new THREE.MeshBasicMaterial({color: 0xff0000})
-  );
-  smallBall.position.set(2, 2, 2);
-//直线光源
-  const pointLight = new THREE.PointLight(0xff0000, 1);
-// pointLight.position.set(2, 2, 2);
-  pointLight.castShadow = true;
-
-// 设置阴影贴图模糊度
-  pointLight.shadow.radius = 20;
-// 设置阴影贴图的分辨率
-  pointLight.shadow.mapSize.set(512, 512);
-  pointLight.decay = 0;
-
-// 设置透视相机的属性
-  smallBall.add(pointLight);
-  scene.add(smallBall);
-
-// gui.add(pointLight.position, "x").min(-5).max(5).step(0.1);
-
-// gui.add(pointLight, "distance").min(0).max(5).step(0.001);
-// gui.add(pointLight, "decay").min(0).max(5).step(0.01);
-
-// 初始化渲染器
-  const renderer = new THREE.WebGLRenderer();
-// 设置渲染的尺寸大小
   renderer.setSize(window.innerWidth, window.innerHeight);
-// 开启场景中的阴影贴图
+
+
+  // 设置色调映射
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 0.5;
   renderer.shadowMap.enabled = true;
   renderer.physicallyCorrectLights = true;
+  // 设置水面效果
 
-// console.log(renderer);
-// 将webgl渲染的canvas内容添加到body
-  // 将webgl渲染的canvas内容添加到body
-  container = document.querySelector('#demoExample');
-  container.appendChild(renderer.domElement);
-
-// // 使用渲染器，通过相机将场景渲染进来
-// renderer.render(scene, camera);
-
-// 创建轨道控制器
+  // 初始化控制器
   const controls = new OrbitControls(camera, renderer.domElement);
-// 设置控制器阻尼，让控制器更有真实效果,必须在动画循环里调用.update()。
+  controls.target.set(-8, 2, 0);
   controls.enableDamping = true;
 
-// 添加坐标轴辅助器
-  const axesHelper = new THREE.AxesHelper(5);
-  scene.add(axesHelper);
-// 设置时钟
-  const clock = new THREE.Clock();
+  // 初始化loader
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("./../draco/");
+
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.setDRACOLoader(dracoLoader);
+
+  // 加载环境纹理
+  let rgbeLoader = new RGBELoader();
+  rgbeLoader.load("./../textures/sky.hdr", (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+  });
+
+  // 加载模型
+  gltfLoader.load("./../model/scene.glb", (gltf) => {
+    const model = gltf.scene;
+    model.traverse((child) => {
+      if (child.name === "Plane") {
+        child.visible = false;
+      }
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    scene.add(model);
+  });
+
+  // 创建水面
+  const waterGeometry = new THREE.CircleGeometry(300, 32);
+  const water = new Water(waterGeometry, {
+    textureWidth: 1024,
+    textureHeight: 1024,
+    color: 0xeeeeff,
+    flowDirection: new THREE.Vector2(1, 1),
+    scale: 100,
+  });
+  water.rotation.x = -Math.PI / 2;
+  water.position.y = -0.4;
+  scene.add(water);
+
+  // 添加平行光
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.set(0, 50, 0);
+  scene.add(light);
+
+  // 添加点光源
+  const pointLight = new THREE.PointLight(0xffffff, 50);
+  pointLight.position.set(0.1, 2.4, 0);
+  pointLight.castShadow = true;
+  scene.add(pointLight);
+
+  // 创建点光源组
+  const pointLightGroup = new THREE.Group();
+  pointLightGroup.position.set(-8, 2.5, -1.5);
+  let radius = 3;
+  let pointLightArr = [];
+  for (let i = 0; i < 3; i++) {
+    // 创建球体当灯泡
+    const sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32);
+    const sphereMaterial = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      emissive: 0xffffff,
+      emissiveIntensity: 10,
+    });
+    const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    pointLightArr.push(sphere);
+    sphere.position.set(
+        radius * Math.cos((i * 2 * Math.PI) / 3),
+        Math.cos((i * 2 * Math.PI) / 3),
+        radius * Math.sin((i * 2 * Math.PI) / 3)
+    );
+
+    let pointLight = new THREE.PointLight(0xffffff, 50);
+    sphere.add(pointLight);
+
+    pointLightGroup.add(sphere);
+  }
+  scene.add(pointLightGroup);
+
+  // 使用补间函数，从0到2π，使灯泡旋转
+  let options = {
+    angle: 0,
+  };
+  gsap.to(options, {
+    angle: Math.PI * 2,
+    duration: 10,
+    repeat: -1,
+    ease: "linear",
+    onUpdate: () => {
+      pointLightGroup.rotation.y = options.angle;
+      pointLightArr.forEach((item, index) => {
+        item.position.set(
+            radius * Math.cos((index * 2 * Math.PI) / 3),
+            Math.cos((index * 2 * Math.PI) / 3 + options.angle * 5),
+            radius * Math.sin((index * 2 * Math.PI) / 3)
+        );
+      });
+    },
+  });
 
   function render() {
-    let time = clock.getElapsedTime();
-    smallBall.position.x = Math.sin(time) * 3;
-    smallBall.position.z = Math.cos(time) * 3;
-    smallBall.position.y = 2 + Math.sin(time * 10) / 2;
-    controls.update();
-    renderer.render(scene, camera);
-    //   渲染下一帧的时候就会调用render函数
     requestAnimationFrame(render);
+    renderer.render(scene, camera);
+    controls.update();
   }
 
   render();
 
-  // 监听画面变化，更新渲染画面
-  window.addEventListener("resize", () => {
-    //   console.log("画面变化了");
-    // 更新摄像头
-    camera.aspect = window.innerWidth / window.innerHeight;
-    //   更新摄像机的投影矩阵
-    camera.updateProjectionMatrix();
+  // 使用补间动画移动相机
+  let timeLine1 = gsap.timeline();
+  let timeline2 = gsap.timeline();
 
-    //   更新渲染器
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    //   设置渲染器的像素比
-    renderer.setPixelRatio(window.devicePixelRatio);
-  });
-})
+  // 定义相机移动函数
+  function translateCamera(position, target) {
+    timeLine1.to(camera.position, {
+      x: position.x,
+      y: position.y,
+      z: position.z,
+      duration: 1,
+      ease: "power2.inOut",
+    });
 
+    timeline2.to(controls.target, {
+      x: target.x,
+      y: target.y,
+      z: target.z,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  }
+
+  let scenes = [
+    {
+      text: "圣诞快乐",
+      callback: () => {
+        // 执行函数切换位置
+        translateCamera(
+            new THREE.Vector3(-3.23, 3, 4.06),
+            new THREE.Vector3(-8, 2, 0)
+        );
+      },
+    },
+    {
+      text: "感谢在这么大的世界里遇见了你",
+      callback: () => {
+        // 执行函数切
+        translateCamera(new THREE.Vector3(7, 0, 23), new THREE.Vector3(0, 0, 0));
+      },
+    },
+    {
+      text: "愿与你探寻世界的每一个角落",
+      callback: () => {
+        // 执行函数切
+        translateCamera(new THREE.Vector3(10, 3, 0), new THREE.Vector3(5, 2, 0));
+      },
+    },
+    {
+      text: "愿将天上的星星送给你",
+      callback: () => {
+        // 执行函数切
+        translateCamera(new THREE.Vector3(7, 0, 23), new THREE.Vector3(0, 0, 0));
+        makeHeart();
+      },
+    },
+    {
+      text: "愿疫情结束，大家健康快乐！",
+      callback: () => {
+        // 执行函数切
+        translateCamera(
+            new THREE.Vector3(-20, 1.3, 6.6),
+            new THREE.Vector3(5, 2, 0)
+        );
+      },
+    },
+  ];
+
+  let index = ref(0);
+  let isAnimate = false;
+  // 监听鼠标滚轮事件
+  window.addEventListener(
+      "wheel",
+      (e) => {
+        if (isAnimate) return;
+        isAnimate = true;
+        if (e.deltaY > 0) {
+          index.value++;
+          if (index.value > scenes.length - 1) {
+            index.value = 0;
+            restoreHeart();
+          }
+        }
+        scenes[index.value].callback();
+        setTimeout(() => {
+          isAnimate = false;
+        }, 1000);
+      },
+      false
+  );
+
+  // 实例化创建漫天星星
+  let starsInstance = new THREE.InstancedMesh(
+      new THREE.SphereGeometry(0.1, 32, 32),
+      new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        emissive: 0xffffff,
+        emissiveIntensity: 10,
+      }),
+      100
+  );
+
+  // 星星随机到天上
+  let starsArr = [];
+  let endArr = [];
+
+  for (let i = 0; i < 100; i++) {
+    let x = Math.random() * 100 - 50;
+    let y = Math.random() * 100 - 50;
+    let z = Math.random() * 100 - 50;
+    starsArr.push(new THREE.Vector3(x, y, z));
+
+    let matrix = new THREE.Matrix4();
+    matrix.setPosition(x, y, z);
+    starsInstance.setMatrixAt(i, matrix);
+  }
+  scene.add(starsInstance);
+
+  // 创建爱心路径
+  let heartShape = new THREE.Shape();
+  heartShape.moveTo(25, 25);
+  heartShape.bezierCurveTo(25, 25, 20, 0, 0, 0);
+  heartShape.bezierCurveTo(-30, 0, -30, 35, -30, 35);
+  heartShape.bezierCurveTo(-30, 55, -10, 77, 25, 95);
+  heartShape.bezierCurveTo(60, 77, 80, 55, 80, 35);
+  heartShape.bezierCurveTo(80, 35, 80, 0, 50, 0);
+  heartShape.bezierCurveTo(35, 0, 25, 25, 25, 25);
+
+  // 根据爱心路径获取点
+  let center = new THREE.Vector3(0, 2, 10);
+  for (let i = 0; i < 100; i++) {
+    let point = heartShape.getPoint(i / 100);
+    endArr.push(
+        new THREE.Vector3(
+            point.x * 0.1 + center.x,
+            point.y * 0.1 + center.y,
+            center.z
+        )
+    );
+  }
+
+  // 创建爱心动画
+  function makeHeart() {
+    let params = {
+      time: 0,
+    };
+
+    gsap.to(params, {
+      time: 1,
+      duration: 1,
+      onUpdate: () => {
+        for (let i = 0; i < 100; i++) {
+          let x = starsArr[i].x + (endArr[i].x - starsArr[i].x) * params.time;
+          let y = starsArr[i].y + (endArr[i].y - starsArr[i].y) * params.time;
+          let z = starsArr[i].z + (endArr[i].z - starsArr[i].z) * params.time;
+          let matrix = new THREE.Matrix4();
+          matrix.setPosition(x, y, z);
+          starsInstance.setMatrixAt(i, matrix);
+        }
+        starsInstance.instanceMatrix.needsUpdate = true;
+      },
+    });
+  }
+
+  function restoreHeart() {
+    let params = {
+      time: 0,
+    };
+
+    gsap.to(params, {
+      time: 1,
+      duration: 1,
+      onUpdate: () => {
+        for (let i = 0; i < 100; i++) {
+          let x = endArr[i].x + (starsArr[i].x - endArr[i].x) * params.time;
+          let y = endArr[i].y + (starsArr[i].y - endArr[i].y) * params.time;
+          let z = endArr[i].z + (starsArr[i].z - endArr[i].z) * params.time;
+          let matrix = new THREE.Matrix4();
+          matrix.setPosition(x, y, z);
+          starsInstance.setMatrixAt(i, matrix);
+        }
+        starsInstance.instanceMatrix.needsUpdate = true;
+      },
+    });
+  }
 </script>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
-<style lang="scss" scoped>
-.three-demoExample {
-  width: 100%;
-  height: 100%;
+#example {
+  width: 100vw;
+  heigh: 100vh;
+}
+
+.scenes {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  pointer-events: none;
+  transition: all 1s;
+}
+
+canvas {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
 }
 </style>
